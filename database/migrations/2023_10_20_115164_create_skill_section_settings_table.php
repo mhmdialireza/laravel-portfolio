31c9d0549f044f9726fad976b11f34ba85305b2f
Alireza Mohammadi <mhmdi.alireza8035@gmail.com>
2023-10-20 15:31:51 +0330

    Add Skill section

---
+++ database/migrations/2023_10_20_115164_create_skill_section_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skill_section_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('sub_title');
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skill_section_settings');
+    }
+};
